f7dbce27eb398f3eb6fc0bb5d62839424b3fe384
Stuart Ervine <spervine@gmail.com>
2014-02-21 10:03:54 +0000

    Initial commit. [SE]

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+CURRENT_USER=`whoami`
+CURRENT_PATH=`pwd`
+XCODE_PATH=`xcode-select -print-path`
+SDK_PATH=`ls -t -d $XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/* | head -1`
+function cleanTargets() {
+    rm -rf ~/Library/Developer/Xcode/DerivedData
+    rm -rf ~/Library/Application Support/iPhone Simulator
+    rm -rf build
+    xcodebuild -target {{FRAMEWORK_NAME}} -sdk iphoneos -configuration Release clean;
+    xcodebuild -target test-unit -sdk $SDK_PATH -configuration Debug clean;
+}
+
+function runTests() {
+    #xcodebuild -target package -sdk iphoneos -configuration Release build
+    xcodebuild -verbose -target test-unit -sdk $SDK_PATH -configuration Debug build
+    OUT=$?
+    if [ $OUT -ne 0 ]
+    then
+        echo "Build FAILED : $OUT";
+        exit 1
+    fi
+}
+
+function buildRelease() {
+    xcodebuild -target package -sdk iphoneos -configuration Debug build
+    OUT=$?
+    if [ $OUT -ne 0 ]
+    then
+        echo "Build FAILED : $OUT";
+        exit 1
+    fi
+}
+
+case "$1" in
+  clean)
+  cleanTargets;;
+
+  test)
+  runTests;;
+
+  release)
+  buildRelease;;
+  *)
+  echo "Usage: build.sh (clean|test|release) e.g. ./build.sh test";
+  exit 1;;
+esac
+
+exit 0
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_PATH=`pwd`
+PROJECT_NAME=$1
+
+if [ "$PROJECT_NAME" == "" ]
+then
+    echo "You must provide a project name. e.g. ./setup.sh Bob"
+    exit 1
+fi
+
+echo "Creating project $PROJECT_NAME"
+cp -R ../OCFramework ../$PROJECT_NAME
+cd ../$PROJECT_NAME
+sed -i.bak "s/{{FRAMEWORK_NAME}}/${PROJECT_NAME}/g" OCFramework.xcodeproj/project.pbxproj
+sed -i.bak "s/{{FRAMEWORK_NAME}}/${PROJECT_NAME}/g" OCFramework.xcodeproj/project.xcworkspace/contents.xcworkspacedata
+
+mv OCFramework.xcodeproj $PROJECT_NAME.xcodeproj
+mv src/main/OCFramework-Info.plist src/main/$PROJECT_NAME-Info.plist
+mv src/main/OCFramework-Prefix.pch src/main/$PROJECT_NAME-Prefix.pch
